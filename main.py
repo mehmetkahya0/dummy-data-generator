314bc22b4df16b60c354ce10887dc4c8d476643c
Mehmet Kahya <mehmetkahyakas5@gmail.com>
2024-08-03 12:42:58 +0300

    chore: Refactor main.py for readability and adherence to PEP8 guidelines

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-# Python Dummy Data Generator for testing purposes only
-# Author: Mehmet Kahya
-# Date: 3-08-2024 DD-MM-YYYY
-# Version: 1.0
-# Description: Generates dummy data for testing purposes only
-# Usage: python main.py
-
 import random
 import string
 import json
 import os
 import sys
-from datetime import datetime
-from datetime import timedelta
-from colorama import Fore, Back, Style
+from datetime import datetime, timedelta
+from colorama import Fore, Style
 import argparse
 
-# Function to generate random string
-
+print(Fore.RED + 'Python Dummy Data Generator')
+print(Style.RESET_ALL)
 
 def random_string(string_length=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(string_length))
 
-# Function to generate random number
-
-
 def random_number():
     return random.randint(1, 100)
-
-# Function to generate random date
-
 
 def random_date():
     start_date = datetime.now()
@@ -40,14 +26,8 @@ def random_date():
     random_date = start_date + timedelta(days=random_number_of_days)
     return random_date.strftime("%Y-%m-%d")
 
-# Function to generate random boolean
-
-
 def random_boolean():
     return random.choice([True, False])
-
-# Function to generate random data
-
 
 def generate_data():
     data = {}
@@ -57,17 +37,11 @@ def generate_data():
     data['is_active'] = random_boolean()
     return data
 
-# Function to write data to file
-
-
 def write_data_to_file(data, file_name):
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=4)
         print(Fore.GREEN + 'Data has been written to file: ' + file_name)
         print(Style.RESET_ALL)
-
-# Function to read data from file
-
 
 def read_data_from_file(file_name):
     with open(file_name, 'r') as file:
@@ -76,13 +50,29 @@ def read_data_from_file(file_name):
         print(Style.RESET_ALL)
         print(data)
 
-# Function to generate data and write to file
-
-
 def generate_data_and_write_to_file(file_name, number):
     data_list = [generate_data() for _ in range(number)]
     write_data_to_file(data_list, file_name)
 
+def calculate_data_size(data):
+    return sys.getsizeof(data)
+
+def calculate_file_size(file_name):
+    return os.path.getsize(file_name)
+
+def calculate_data_and_file_size(data, file_name):
+    data_size = calculate_data_size(data)
+    file_size = calculate_file_size(file_name)
+    print(Fore.RESET + 'Data Size: ' + str(data_size) + ' bytes')
+    print('File Size: ' + str(file_size) + ' bytes')
+    print(Style.RESET_ALL)
+
+def calculate_runtime(start_time, end_time):
+    runtime = end_time - start_time
+    print(Fore.BLUE + 'Start Time: ' + str(start_time))
+    print('End Time: ' + str(end_time))
+    print('Runtime: ' + str(runtime.total_seconds()) + ' seconds')
+    print(Style.RESET_ALL)
 
 def main():
     parser = argparse.ArgumentParser(description='Python Dummy Data Generator')
@@ -94,11 +84,17 @@ def main():
         '-n', '--number', help='Number of data to generate', type=int, default=1)
     args = parser.parse_args()
 
+    start_time = datetime.now()
+
     if args.read:
         read_data_from_file(args.file)
+        calculate_data_and_file_size(read_data_from_file(args.file), args.file)
     else:
         generate_data_and_write_to_file(args.file, args.number)
+        calculate_data_and_file_size(generate_data(), args.file)
 
+    end_time = datetime.now()
+    calculate_runtime(start_time, end_time)
 
 if __name__ == '__main__':
     main()
